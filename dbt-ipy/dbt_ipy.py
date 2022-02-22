ff4de8093ea4c6c31555c2a9a57f8b41c7daf720
Jose Riego Valenzuela <jvalenzuela@indeed.com>
2022-02-22 08:49:26 +0100

    add dbt_clipboard line magic

--- dbt-ipy/dbt_ipy.py
+++ dbt-ipy/dbt_ipy.py
@@ -9,6 +9,8 @@ import socket
 import subprocess
 import time
 from typing import Any, Dict, List, Optional, Union
+import tempfile
+import pyperclip
 
 from IPython.core.magic import Magics, cell_magic, line_magic, magics_class
 from agate import Table
@@ -418,6 +420,7 @@ class DBTMagics(Magics):
         # You must call the parent constructor
         super(DBTMagics, self).__init__(shell)
         atexit.register(self._stop)
+        self.last_result = None
 
     def _stop(self):
         try:
@@ -450,6 +453,7 @@ class DBTMagics(Magics):
         rows_dict = [dict(zip(column_names, row)) for row in resp_table['rows']]
         table_result = Table.from_object(rows_dict)
         table_result.print_table()
+        self.last_result = table_result
         return table_result
 
     @cell_magic
@@ -458,4 +462,18 @@ class DBTMagics(Magics):
         resp = self.querier.compile_sql(query)
         result = self.querier.async_wait_for_result(resp)
         compiled_sql = result['results'][0]['compiled_sql']
+        self.last_result = compiled_sql
         return Sql(compiled_sql)
+
+    @line_magic
+    def dbt_clipboard(self, line):
+        if isinstance(self.last_result, Table):
+            try:
+                fd, path = tempfile.mkstemp()
+                self.last_result.to_csv(path)
+                with open(path) as f:
+                    pyperclip.copy(f.read())
+            finally:
+                os.remove(path)
+        else:
+            pyperclip.copy(self.last_result)
